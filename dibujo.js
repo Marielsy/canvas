e06534253124ba3f4373bb88b52b2478601143fe
Marielsy <carvajalmarielsy@gmail.com>
2021-02-21 22:16:59 -0500

    comentarios

--- dibujo.js
+++ dibujo.js
@@ -1,22 +1,11 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0 ;
-var yi , xf;
-var colorcito = "blue";
-
-//otro comentario victor
- for (l=0; l < lineas ; l++)
- {
-   yi = 10 * l;
-   xf = 10 * ( l + 1);
-   dibujarLinea(colorcito, 0, yi, xf, 300);
-   console.log( "linea" + l);
-
- }
-
-  dibujarLinea(colorcito, 2, 2, 2, 300);
-  dibujarLinea(colorcito, 2, 299, 299, 299);
 
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
@@ -29,5 +18,25 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.closePath();
 }
 
-//Comentario de Victor. 
-//Esto es una prueba
+function dibujoPorClick()
+{
+    var lineas = parseInt(texto.value);
+    var l = 0 ;
+    var yi , xf;
+    var colorcito = "blue";
+    var espacio = ancho/ lineas;
+
+
+     for (l=0; l < lineas ; l++)
+     {
+       yi = espacio * l;
+       xf = espacio * ( l + 1);
+       dibujarLinea(colorcito, 0, yi, xf, 300);
+       console.log( "linea" + l);
+
+     }
+
+      dibujarLinea(colorcito, 2, 2, 2, 300);
+      dibujarLinea(colorcito, 2, 299, 299, 299);
+
+}
